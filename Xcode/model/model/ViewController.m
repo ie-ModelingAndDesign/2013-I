9678e3bdba48981366af98734f4d644739ef0352
Nariaki UEMA <e125720@N-no-MacBook-Pro.local>
2014-01-13 20:06:13 +0900

    wall add program

--- Xcode/model/model/ViewController.m
+++ Xcode/model/model/ViewController.m
@@ -466,30 +466,151 @@ int oprum = 10;      // どの位置にボタンを表示させるかに使用�
                         [UIImage imageNamed:@"rennga6.jpg"],
                         [UIImage imageNamed:@"rennga7.jpg"],
                         [UIImage imageNamed:@"rennga8.jpg"],
-                        [UIImage imageNamed:@"rennga9.jpg"]
+                        [UIImage imageNamed:@"rennga9.jpg"],
+                        [UIImage imageNamed:@"rennga10.jpg"],
+                        [UIImage imageNamed:@"rennga11.jpg"],
+                        [UIImage imageNamed:@"rennga12.jpg"],
+                        [UIImage imageNamed:@"rennga13.jpg"],
+                        [UIImage imageNamed:@"rennga14.jpg"],
+                        [UIImage imageNamed:@"rennga15.jpg"],
+                        [UIImage imageNamed:@"rennga16.jpg"],
+                        [UIImage imageNamed:@"rennga17.jpg"],
+                        [UIImage imageNamed:@"rennga18.jpg"],
+                        [UIImage imageNamed:@"rennga19.jpg"],
+                        [UIImage imageNamed:@"rennga20.jpg"],
+                        [UIImage imageNamed:@"rennga21.jpg"],
+                        [UIImage imageNamed:@"rennga22.jpg"],
+                        [UIImage imageNamed:@"rennga23.jpg"],
+                        [UIImage imageNamed:@"rennga24.jpg"],
+                        [UIImage imageNamed:@"rennga25.jpg"],
+                        [UIImage imageNamed:@"rennga26.jpg"],
+                        [UIImage imageNamed:@"rennga27.jpg"],
+                        [UIImage imageNamed:@"rennga28.jpg"],
+                        [UIImage imageNamed:@"rennga29.jpg"],
+                        [UIImage imageNamed:@"rennga30.jpg"],
+                        [UIImage imageNamed:@"rennga31.jpg"],
+                        [UIImage imageNamed:@"rennga32.jpg"],
+                        [UIImage imageNamed:@"rennga33.jpg"],
+                        [UIImage imageNamed:@"rennga34.jpg"],
+                        [UIImage imageNamed:@"rennga35.jpg"],
+                        [UIImage imageNamed:@"rennga36.jpg"],
+                        [UIImage imageNamed:@"rennga37.jpg"],
+                        [UIImage imageNamed:@"rennga38.jpg"],
+                        [UIImage imageNamed:@"rennga39.jpg"],
+                        [UIImage imageNamed:@"rennga40.jpg"],
+                        [UIImage imageNamed:@"rennga41.jpg"],
+                        [UIImage imageNamed:@"rennga42.jpg"],
+                        [UIImage imageNamed:@"rennga43.jpg"],
+                        [UIImage imageNamed:@"rennga44.jpg"],
+                        [UIImage imageNamed:@"rennga45.jpg"],
+                        [UIImage imageNamed:@"rennga46.jpg"],
+                        [UIImage imageNamed:@"rennga47.jpg"],
+                        [UIImage imageNamed:@"rennga48.jpg"],
+                        [UIImage imageNamed:@"rennga49.jpg"],
+                        [UIImage imageNamed:@"rennga50.jpg"]
                         ];
     
     //int ia = (1000000 - ie) % 100;
-    if(ie == 0) {
+    if(0 <= ie && ie < 100) {
         imageView.image = images[0];
-    } else if(ie == 100000){
+    } else if(100 <= ie && ie < 200){
         imageView.image = images[1];
-    } else if(ie == 200000){
+    } else if(200 <= ie && ie < 300){
         imageView.image = images[2];
-    } else if(ie == 300000){
+    } else if(300 <= ie && ie < 400){
         imageView.image = images[3];
-    } else if(ie == 400000){
+    } else if(400 <= ie && ie < 500){
         imageView.image = images[4];
-    } else if(ie == 500000){
+    } else if(500 <= ie && ie < 800){
         imageView.image = images[5];
-    } else if(ie == 600000){
+    } else if(800 <= ie && ie < 1000){
         imageView.image = images[6];
-    } else if(ie == 700000){
+    } else if(1000 <= ie && ie < 10000){
         imageView.image = images[7];
-    } else if(ie == 800000){
+    } else if(10000 <= ie && ie < 30000){
         imageView.image = images[8];
-    } else if(ie == 900000){
+    } else if(30000 <= ie && ie < 50000){
         imageView.image = images[9];
+    } else if(50000 <= ie && ie < 100000){
+        imageView.image = images[10];
+    } else if(100000 <= ie && ie < 150000){
+        imageView.image = images[11];
+    } else if(150000 <= ie && ie < 200000){
+        imageView.image = images[12];
+    } else if(200000 <= ie && ie < 250000){
+        imageView.image = images[13];
+    } else if(250000 <= ie && ie < 300000){
+        imageView.image = images[14];
+    } else if(300000 <= ie && ie < 350000){
+        imageView.image = images[15];
+    } else if(350000 <= ie && ie < 400000){
+        imageView.image = images[16];
+    } else if(400000 <= ie && ie < 450000){
+        imageView.image = images[17];
+    } else if(450000 <= ie && ie < 500000){
+        imageView.image = images[18];
+    } else if(500000 <= ie && ie < 550000){
+        imageView.image = images[19];
+    } else if(550000 <= ie && ie < 600000){
+        imageView.image = images[20];
+    } else if(600000 <= ie && ie < 650000){
+        imageView.image = images[21];
+    } else if(650000 <= ie && ie < 700000){
+        imageView.image = images[22];
+    } else if(700000 <= ie && ie < 750000){
+        imageView.image = images[23];
+    } else if(750000 <= ie && ie < 800000){
+        imageView.image = images[24];
+    } else if(800000 <= ie && ie < 850000){
+        imageView.image = images[25];
+    } else if(850000 <= ie && ie < 860000){
+        imageView.image = images[26];
+    } else if(860000 <= ie && ie < 870000){
+        imageView.image = images[27];
+    } else if(870000 <= ie && ie < 880000){
+        imageView.image = images[28];
+    } else if(880000 <= ie && ie < 890000){
+        imageView.image = images[29];
+    } else if(890000 <= ie && ie < 900000){
+        imageView.image = images[30];
+    } else if(900000 <= ie && ie < 901000){
+        imageView.image = images[31];
+    } else if(901000 <= ie && ie < 902000){
+        imageView.image = images[32];
+    } else if(902000 <= ie && ie < 903000){
+        imageView.image = images[33];
+    } else if(903000 <= ie && ie < 904000){
+        imageView.image = images[34];
+    } else if(904000 <= ie && ie < 905000){
+        imageView.image = images[35];
+    } else if(905000 <= ie && ie < 908000){
+        imageView.image = images[36];
+    } else if(908000 <= ie && ie < 910000){
+        imageView.image = images[37];
+    } else if(910000 <= ie && ie < 930000){
+        imageView.image = images[38];
+    } else if(930000 <= ie && ie < 950000){
+        imageView.image = images[39];
+    } else if(950000 <= ie && ie < 970000){
+        imageView.image = images[40];
+    } else if(970000 <= ie && ie < 990000){
+        imageView.image = images[41];
+    } else if(990000 <= ie && ie < 994000){
+        imageView.image = images[42];
+    } else if(994000 <= ie && ie < 998000){
+        imageView.image = images[43];
+    } else if(998000 <= ie && ie < 999500){
+        imageView.image = images[44];
+    } else if(999500 <= ie && ie < 999600){
+        imageView.image = images[45];
+    } else if(999600 <= ie && ie < 999700){
+        imageView.image = images[46];
+    } else if(999700 <= ie && ie < 999800){
+        imageView.image = images[47];
+    } else if(999800 <= ie && ie < 999900){
+        imageView.image = images[48];
+    } else if(999900 <= ie){
+        imageView.image = images[49];
     }
 
     
